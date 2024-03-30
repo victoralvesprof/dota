ea8406043b46f5aeedbc89c4346be80dcb3e337f
victoralvesprof <victoralves.prof@gmail.com>
2024-03-30 09:32:01 -0300

    test: add test get all heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from 'layout';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LayoutModule],
+  imports: [RouterModule, LayoutModule, HttpClientModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
